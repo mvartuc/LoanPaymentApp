4a92c0d1d34da735a751b5dd428149b186a01f95
Lorenium <mva.artuc@gmail.com>
2023-01-24 13:48:52 +0300

    added an input field to the form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ParentCategory } from 'src/app/models/category';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  @Input() categories?: ParentCategory[];
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
