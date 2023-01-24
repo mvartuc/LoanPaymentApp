c5ad57d47d42339765449c1769737f86dc36f74a
Lorenium <mva.artuc@gmail.com>
2023-01-24 12:23:22 +0300

    created a service get function

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { ParentCategory } from '../models/category';
 
 @Injectable({
@@ -8,5 +9,10 @@ import { ParentCategory } from '../models/category';
 })
 export class CategoryService {
   private url = 'Category';
+  private prefix = `${environment.apiUrl}/${this.url}`
   constructor(private http: HttpClient) {}
+
+  public getParentCategories(): Observable<ParentCategory[]> {
+    return this.http.get<ParentCategory[]>(`${this.prefix}/GetParents`);
+  }
 }
