e4c719198ad57d7373f66d5631f0206f985fc873
Lorenium <mva.artuc@gmail.com>
2023-01-24 13:48:19 +0300

    subscribed to api get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ParentCategory } from './models/category';
+import { CategoryService } from './services/category.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'StajProjesi';
+  categories: ParentCategory[] = [];
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService
+      .getParentCategories()
+      .subscribe((result: ParentCategory[]) => (this.categories = result));
+  }
 }
