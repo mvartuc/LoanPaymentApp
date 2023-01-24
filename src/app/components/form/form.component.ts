7ff16936cd93d1268de1f35fef5caa1623b4b8ca
Lorenium <mva.artuc@gmail.com>
2023-01-24 17:28:50 +0300

    added a simple product category dropdown

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ParentCategory } from 'src/app/models/category';
+import { ChildCategory, ParentCategory } from 'src/app/models/category';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
   @Input() categories?: ParentCategory[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  selectCategory(childCategory:ChildCategory): void {
+    console.log(childCategory);
   }
-
 }
