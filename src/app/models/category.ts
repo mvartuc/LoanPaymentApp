85d5cb60ff72b739efd02a2a8a0b2d90f3c35d1a
Lorenium <mva.artuc@gmail.com>
2023-01-25 16:52:20 +0300

    refactored models removed displayName removed category enums

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,28 +1,15 @@
-export enum ParentCategoryEnum {
-  AdatliKredi,
-  TaksitliKredi,
-}
-export enum ChildCategoryEnum {
-  RotatifKredi,
-  IsletmeIhtiyacKredisi,
-}
-
-export class ChildCategory {
+export class Category {
   id?: number;
   code: number = 0;
   name: String = '';
-  displayName: String = '';
   description: String = '';
-  childCategoryEnum: ChildCategoryEnum = 0;
-  parentCategoryID?: number;
+  parentCategoryID!: number;
 }
 
 export class ParentCategory {
   id?: number;
   code: number = 0;
   name: String = '';
-  displayName: String = '';
   description: String = '';
-  parentCategoryEnum: ParentCategoryEnum = 0;
-  childCategories: ChildCategory[] = [];
+  childCategories: Category[] = [];
 }
