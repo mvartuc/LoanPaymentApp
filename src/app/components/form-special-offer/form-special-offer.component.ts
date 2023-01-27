4abdddfd9d13fbc3c26caa888a48f45ba64a5f3b
Lorenium <mva.artuc@gmail.com>
2023-01-27 16:09:10 +0300

    implemented the special offer component

--- src/app/components/form-special-offer/form-special-offer.component.ts
+++ src/app/components/form-special-offer/form-special-offer.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/category';
+import { SpecialOffer } from 'src/app/models/special-offer';
+import { SpecialOfferService } from 'src/app/services/special-offer.service';
 
 @Component({
   selector: 'app-form-special-offer',
   templateUrl: './form-special-offer.component.html',
-  styleUrls: ['./form-special-offer.component.scss']
+  styleUrls: ['./form-special-offer.component.scss'],
 })
-export class FormSpecialOfferComponent implements OnInit {
+export class FormSpecialOfferComponent implements OnInit, OnChanges {
+  public specialOffers$!: Observable<SpecialOffer[]>;
+  @Input() public category!: Category;
+  @Input() public legend!: string;
+  selectedOffer?: SpecialOffer;
 
-  constructor() { }
+  constructor(private specialOfferService: SpecialOfferService) {}
 
   ngOnInit(): void {
+    this.refreshOffers();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['category']) {
+      this.refreshOffers();
+    }
+  }
+
+  refreshOffers() {
+    this.specialOffers$ = this.specialOfferService.getSpecialOffers(
+      this.category.code
+    );
+  }
+
+  // getOffers(): SpecialOffer[] {
+  //   // return this.specialOffers.filter(
+  //   //   (o) => o.productCategoryId === this.category.id
+  //   // );
+
+  //   return this.specialOffers;
+  // }
 }
