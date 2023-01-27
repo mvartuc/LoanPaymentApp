6bc5fa340687306a950765653b97c908683859a7
Lorenium <mva.artuc@gmail.com>
2023-01-27 10:26:04 +0300

    used the parameter service in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ParentCategory } from './models/category';
+import { Parameter } from './models/parameter';
 import { CategoryService } from './services/category.service';
+import { ParameterService } from './services/parameter.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,20 @@ import { CategoryService } from './services/category.service';
 export class AppComponent {
   title = 'StajProjesi';
   categories: ParentCategory[] = [];
+  parameters: Parameter[] = [];
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    private parameterService: ParameterService
+  ) {}
 
   ngOnInit(): void {
     this.categoryService
       .getParentCategories()
       .subscribe((result: ParentCategory[]) => (this.categories = result));
+
+    this.parameterService
+      .getAllParameters()
+      .subscribe((result: Parameter[]) => (this.parameters = result));
   }
 }
