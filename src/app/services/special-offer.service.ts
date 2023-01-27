86d294b95aa3718aa8d4e17e01cfc724aba044ca
Lorenium <mva.artuc@gmail.com>
2023-01-27 16:08:15 +0300

    implemented special-offer service

--- src/app/services/special-offer.service.ts
+++ src/app/services/special-offer.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { SpecialOffer } from '../models/special-offer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpecialOfferService {
+  private url = 'SpecialOffer';
+  private prefix = `${environment.apiUrl}/${this.url}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getSpecialOffers(categoryCode:string): Observable<SpecialOffer[]> {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("categoryCode", categoryCode)
+    return this.http.get<SpecialOffer[]>(`${this.prefix}/GetOffers`, {params: queryParams});
+  }
 }
