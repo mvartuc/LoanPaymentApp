f5fdcd8d809731a5bb4374f2410282ba8bcd087f
Lorenium <mva.artuc@gmail.com>
2023-01-27 10:24:26 +0300

    implemented the paramter service

--- src/app/services/parameter.service.ts
+++ src/app/services/parameter.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Parameter } from '../models/parameter';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ParameterService {
+  private url = 'Parameter';
+  private prefix = `${environment.apiUrl}/${this.url}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getAllParameters(): Observable<Parameter[]> {
+    return this.http.get<Parameter[]>(`${this.prefix}/GetParams`);
+  }
+
+  public getAllParametersByGroupCode(groupCode: string, params: Parameter[]): Parameter[] {
+    return params.filter((p) => p.groupCode === groupCode);
+  }
 }
