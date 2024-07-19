886b4fd21c56f6d6117df47d7f0d9ce8d6abb52b
mvartuc <dev.mvartuc@gmail.com>
2024-07-19 21:41:43 +0200

    Changed client CORS url to https

--- LoanPaymentApp.UI/src/environments/environment.prod.ts
+++ LoanPaymentApp.UI/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://x4sksgo:7217/api"
+  apiUrl: "https://x4sksgo:7217/api"
 };
--- LoanPaymentApp.UI/src/environments/environment.ts
+++ LoanPaymentApp.UI/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://x4sksgo:7217/api"
+  apiUrl: "https://x4sksgo:7217/api"
 };
 
 /*
